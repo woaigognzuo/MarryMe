7e6f0708cfab4400d2a3698932e86a84c801e38f
wenxingjiang <1677970371@qq.com>
2022-03-08 16:42:59 +0800

    根据忽略文件控制代码

---
+++ MarryMe/Source/MarryMe.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class MarryMeTarget : TargetRules
+{
+	public MarryMeTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("MarryMe");
+	}
+}
